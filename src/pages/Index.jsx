916a457761942e83782081c9d288c11e1fb222aa
mikyung <seomk08@gmail.com>
2025-04-06 02:55:51 +0900

    chore: 헤더 컴포넌트 임포트트

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,9 @@
+import Header from '../components/Header';
+
 const Index = () => {
   return (
     <>
+      <Header />
       <div className="text-center p-10">
         <h1 className="text-2xl font-bold">Index Page</h1>
       </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,9 @@
+import Header from '../components/Header';
+
 const Main = () => {
   return (
     <>
+      <Header />
       <div className="text-center p-10">
         <h1 className="text-2xl font-bold">Main Page</h1>
       </div>
