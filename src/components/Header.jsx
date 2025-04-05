2e72f7ff601bae3ea808c7996955784e90d60b00
mikyung <seomk08@gmail.com>
2025-04-06 02:55:23 +0900

    chore: 공통 컴포넌트 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold">Header</h1>
+    </div>
+  );
+};
+export default Header;
