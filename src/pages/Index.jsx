c7c70e5b930644c3788bb8d4643651161a31c214
mikyung <seomk08@gmail.com>
2025-04-07 19:06:27 +0900

    chore: tailwind 커스텀색상 추가 및 수정정

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,10 +4,11 @@ const Index = () => {
   return (
     <>
       <Header />
-      <div className="text-center p-10">
-        <h1 className="text-2xl font-bold">Index Page</h1>
+      <div className="text-center p-10 space-y-4">
+        <h1 className="text-2xl font-bold mb-4 bg-main/100 text-white p-3 rounded">Index Page</h1>
       </div>
     </>
   );
 };
+
 export default Index;
