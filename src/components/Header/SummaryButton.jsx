7ad25207886f2af9ce03221f05c124dd36b9932f
mikyung <seomk08@gmail.com>
2025-04-09 00:39:08 +0900

    refactor: 필터 요약 버튼 UI 대략적 설정

--- src/components/Header/SummaryButton.jsx
+++ src/components/Header/SummaryButton.jsx
@@ -1,6 +1,9 @@
+import { IconSummaryBlack } from '../../utils/icons';
+
 const SummaryButton = () => {
   return (
-    <div>
+    <div className="flex gap-[10px]">
+      <img className="w-[12px] h-auto" src={IconSummaryBlack} alt="filter 아이콘" />
       <div>요약</div>
     </div>
   );
