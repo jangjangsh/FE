bf020ae3c1069feb863a4eb4fab42ab442397999
mikyung <seomk08@gmail.com>
2025-04-09 00:57:44 +0900

    refactor: Header fixed하여 위에고정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,25 @@ import HeaderProfile from './Header/HeaderProfile';
 
 const Header = () => {
   return (
-    <div className="w-full h-[60px] max-w-[1440px] mx-auto bg-white flex items-center border-gray/5 border-b py-[14px]">
-      <div className="px-[20px]">
-        <SidebarToggleButton />
-      </div>
-      {/* 조건부렌더링 */}
-      <div className="flex w-full mx-auto items-center justify-between px-[42px]">
-        <ChatTitle />
-        {/* <div className="flex ml-auto gap-[16px]">
+    <div className="fixed top-0 left-0 w-full h-[60px] bg-white z-50 border-b border-gray-900/5">
+      <div className="max-w-[1440px] mx-auto flex items-center justify-between h-full px-[20px]">
+        {/* 왼쪽 버튼 */}
+        <div className="px-[20px]">
+          <SidebarToggleButton />
+        </div>
+        {/* 조건부렌더링 */}
+        <div className="flex w-full mx-auto items-center justify-between px-[42px]">
+          <ChatTitle />
+          {/* <div className="flex ml-auto gap-[16px]">
           <FilterButton />
           <SummaryButton />
         </div> */}
-      </div>
-      {/* 여기까지 */}
-      {/* <div className="ml-auto pr-[26px]">
+        </div>
+        {/* 여기까지 */}
+        {/* <div className="ml-auto pr-[26px]">
         <HeaderProfile />
       </div> */}
+      </div>
     </div>
   );
 };
