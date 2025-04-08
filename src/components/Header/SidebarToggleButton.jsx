1ff2d81e80032d8a47600a7f7fba954c747ae472
mikyung <seomk08@gmail.com>
2025-04-09 00:44:59 +0900

    refactor: 사이드바 아이콘 추가 및 변수명 오류 수정

--- src/components/Header/SidebarToggleButton.jsx
+++ src/components/Header/SidebarToggleButton.jsx
@@ -1,8 +1,9 @@
-const SidebarToggleButto = () => {
+import { IconMenu } from '../../utils/icons';
+const SidebarToggleButton = () => {
   return (
     <div>
-      <div>tog</div>
+      <img className="w-[21px] h-[14px]" src={IconMenu} alt="사이드바 버튼" />
     </div>
   );
 };
-export default SidebarToggleButto;
+export default SidebarToggleButton;
