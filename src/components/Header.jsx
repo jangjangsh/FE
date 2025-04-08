5ed2f1d13835b40b69660be3ee1101d7918ba9aa
mikyung <seomk08@gmail.com>
2025-04-08 23:58:57 +0900

    feat: 컴포넌트 임포트 및 UI 설정 (대략적)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import SidebarToggleButton from './Header/SidebarToggleButton';
+import ChatTitle from './ChatTitle';
+import FilterButton from './Header/FilterButton';
+import SummaryButton from './Header/SummaryButton';
+import HeaderProfile from './Header/HeaderProfile';
+
 const Header = () => {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-2xl font-bold">Header</h1>
+    <div className="w-full h-[60px] max-w-[1440px] mx-auto bg-white flex items-center border-gray/5 border-b py-[14px]">
+      <div className="px-[20px]">
+        <SidebarToggleButton />
+      </div>
+      {/* 조건부렌더링 */}
+      <div className="flex w-full mx-auto items-center justify-between px-[42px]">
+        <ChatTitle />
+        <div className="flex ml-auto gap-[16px]">
+          <FilterButton />
+          <SummaryButton />
+        </div>
+      </div>
+      {/* 여기까지 */}
+      <div className="ml-auto pr-[26px]">
+        {/* w-[32px] h-[32px] */}
+        <HeaderProfile />
+      </div>
     </div>
   );
 };
