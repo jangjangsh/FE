a623ceff997b5acadf603a2e3453840f87a78d60
mikyung <seomk08@gmail.com>
2025-04-09 20:15:50 +0900

    style: sidebar랑 ui 맞추기 위한 css수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-[60px] bg-white z-50 border-b border-gray-900/5">
-      <div className="max-w-[1440px] mx-auto flex items-center justify-between h-full">
+      <div className=" mx-auto flex items-center justify-between h-full">
         {/* 왼쪽 버튼 */}
         <div className="px-[20px]">
           <SidebarToggleButton />
@@ -22,15 +22,15 @@ const Header = () => {
           {/* currentSessionId가 있을 때만 ChatTitle 보여줌 */}
           {currentSessionId && <ChatTitle isHeader={true} />}
 
-          {/* <div className="flex ml-auto gap-[16px]">
-          <FilterButton />
-          <SummaryButton />
-        </div> */}
+          <div className="flex ml-auto gap-[16px]">
+            <FilterButton />
+            <SummaryButton />
+          </div>
         </div>
         {/* 여기까지 */}
-        {/* <div className="ml-auto pr-[26px]">
-        <HeaderProfile />
-      </div> */}
+        <div className="ml-auto pr-[26px]">
+          <HeaderProfile />
+        </div>
       </div>
     </div>
   );
