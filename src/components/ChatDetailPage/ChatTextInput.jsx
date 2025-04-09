48e24a8e7a39a3ecdeeb1840c81563c371b257d6
장서현 <jsh030213@gmail.com>
2025-04-10 00:12:58 +0900

    refactor: textarea maxRows 수정 및 ChatMainPage width 수정

--- src/components/ChatDetailPage/ChatTextInput.jsx
+++ src/components/ChatDetailPage/ChatTextInput.jsx
@@ -5,7 +5,7 @@ const ChatTextInput = () => {
     <div className="flex w-full items-center">
       <TextareaAutosize
         minRows={1}
-        maxRows={8} // 8줄까지만 높이 확장 후 스크롤
+        maxRows={7} // 8줄까지만 높이 확장 후 스크롤
         placeholder="질문을 입력해주세요."
         className="
         inline m-[6px]
--- src/pages/ChatMainPage.jsx
+++ src/pages/ChatMainPage.jsx
@@ -6,7 +6,7 @@ const ChatMainPage = () => {
   return (
     <div className="flex flex-col h-screen w-screen items-center">
       <Header />
-      <div className="flex flex-col justify-center items-center h-3/5 w-[760px]">
+      <div className="flex flex-col justify-center items-center h-4/6 w-[760px]">
         <div className="text-[32px] mb-6 font-bold tracking-wide leading-snug text-center w-full">
           제품 반응 분석, <br />
           <span className="text-main">스포이드</span>로 추출하세요.
