398f63c6ad2daa9fe6a3e369e638f51f7c783653
장서현 <jsh030213@gmail.com>
2025-04-09 21:36:42 +0900

    chore: placeholder 문구 변경

--- src/components/ChatDetailPage/ChatTextInput.jsx
+++ src/components/ChatDetailPage/ChatTextInput.jsx
@@ -6,7 +6,7 @@ const ChatTextInput = () => {
       <TextareaAutosize
         minRows={1}
         maxRows={8} // 8줄까지만 높이 확장 후 스크롤
-        placeholder="무엇을 추출할까요?"
+        placeholder="질문을 입력해주세요."
         className="
         inline m-[6px]
         appearance-none border-none outline-none resize-none
