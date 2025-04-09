6e90cf3d4228cea8432e972138f2ea401a9ae10d
mikyung <seomk08@gmail.com>
2025-04-10 03:43:03 +0900

    style: 새채팅 버튼 UI 완료

--- src/components/SideBar/NewChatButton.jsx
+++ src/components/SideBar/NewChatButton.jsx
@@ -1,7 +1,14 @@
+import { IconPlus } from '../../utils/icons';
+
 const NewChatButton = () => {
   return (
-    <div className="w-[220px] h-[38px]">
-      <div>새 채팅</div>
+    <div
+      className="flex w-[220px] h-[38px] px-[10px] py-[8px] 
+    rounded-[15px] gap-[12px] text-main
+    hover:bg-main-newChatHover "
+    >
+      <img src={IconPlus} alt="채팅추가버튼" />
+      <div className="font-bold">새 채팅</div>
     </div>
   );
 };
