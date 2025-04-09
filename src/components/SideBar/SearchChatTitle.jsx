e878e19425d556fd657875cef924d17562a0dac4
mikyung <seomk08@gmail.com>
2025-04-10 03:59:37 +0900

    style: 채팅제목검색 버튼 UI 완성

--- src/components/SideBar/SearchChatTitle.jsx
+++ src/components/SideBar/SearchChatTitle.jsx
@@ -1,7 +1,14 @@
+import { IconSearch } from '../../utils/icons';
+
 const SearchChatTitle = () => {
   return (
-    <div>
-      <div>검색</div>
+    <div
+      className="flex w-[220px] h-[38px] px-[10px] py-[8px] 
+    rounded-[15px] gap-[12.2px]
+    hover:bg-gray-stroke03"
+    >
+      <img className="w-[20px] h-[20px]" src={IconSearch} alt="검색아이콘" />
+      <div className="text-gray-stroke30 font-medium">채팅 제목 검색</div>
     </div>
   );
 };
