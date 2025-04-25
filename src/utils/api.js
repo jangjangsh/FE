b6cc3f83c314e40eaacdae3fe0bb33b15a7a39bd
장서현 <jsh030213@gmail.com>
2025-04-25 22:13:35 +0900

    feat: headers에 json 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.sspoid.site',
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
 });
 
