9aa4cf7812d9cfaa1ddbdbacd4393f6c37445be3
장서현 <jsh030213@gmail.com>
2025-04-27 20:21:43 +0900

    style: pl 지우기

--- src/pages/ChatDetailPage.jsx
+++ src/pages/ChatDetailPage.jsx
@@ -34,11 +34,11 @@ const ChatDetailPage = () => {
 
       {/* 본문 전체: 헤더 제외 + InputBox 제외 */}
       <div className="pt-[60px] pb-[100px] h-[calc(100vh-60px)] flex overflow-hidden">
-        {/* 사이드바 */}
         <SideBar />
+        {/* 사이드바 */}
 
         {/* 채팅영역 */}
-        <div className="flex-1 overflow-y-auto flex justify-center pl-[16px]">
+        <div className="flex-1 overflow-y-auto flex justify-center">
           <div className="w-[760px]">
             <ChatSection
               sessionId={sessionId}
@@ -50,7 +50,7 @@ const ChatDetailPage = () => {
       </div>
 
       {/* 고정된 InputBox */}
-      <div className="fixed bottom-3 w-full flex justify-center bg-white z-50">
+      <div className="fixed bottom-3 w-full flex justify-center bg-white z-20">
         <div className="w-[760px]">
           <ChatInputBox sessionId={sessionId} fetchMessagesAgain={getBotChat} />
         </div>
