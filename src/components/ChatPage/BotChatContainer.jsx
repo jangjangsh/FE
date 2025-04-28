d778f4f344a7ca7550e91a66719edac8222aff2f
장서현 <jsh030213@gmail.com>
2025-04-28 21:07:27 +0900

    style: 동적으로 움직이는 느낌 추가

--- src/components/ChatPage/BotChatContainer.jsx
+++ src/components/ChatPage/BotChatContainer.jsx
@@ -14,6 +14,7 @@ const BotChatContainer = ({ botMessages }) => {
 
   // 첫 번째 bot 메세지의 skinType을 기준으로 activeType 설정
   const [activeType, setActiveType] = useState(botMessages[0].skinType);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleFilterSelect = (type) => {
     setActiveType(type);
@@ -22,23 +23,34 @@ const BotChatContainer = ({ botMessages }) => {
   return (
     <div className="flex flex-col w-full">
       {/* 선택 버튼 */}
-      <div className="flex w-full gap-0.5 bg-main-chatFilter p-[4px] rounded-t-[10px] border border-main-buttonFill">
+      <div className="relative flex w-full gap-0.5 bg-main-chatFilter p-[4px] rounded-t-[10px] border border-main-buttonFill">
+        {/* 하이라이트 배경판 (추가!!) */}
+        <div
+          className="absolute top-[4px] left-[4px] h-[calc(100%-8px)] bg-white rounded-[8px] transition-transform duration-300 ease-in-out"
+          style={{
+            width: 'calc((100% - 8px) / 4)', // 버튼 폭 기준
+            transform: `translateX(calc(${activeIndex * 100}%)`,
+          }}
+        />
         {botMessages.map((msg, idx) => (
           <button
             key={idx}
-            onClick={() => handleFilterSelect(msg.skinType)}
+            onClick={() => {
+              handleFilterSelect(msg.skinType);
+              setActiveIndex(idx);
+            }}
             className={`
               ${
                 activeType === msg.skinType
-                  ? 'bg-white text-main border border-main-chatFilterStroke'
-                  : 'text-main-buttonStroke border border-main-chatFilter hover:bg-main-typeBackground hover:text-main-chatFilterHover duration-200 hover:border-main-chatFilter'
+                  ? 'z-10 text-main'
+                  : 'z-10 text-main-buttonStroke hover:bg-main-typeBackground hover:text-main-chatFilterHover duration-200 hover:border-main-chatFilter'
               }
               
               [width:calc((100%-6px)/4)]
               items-center justify-center
                 py-[5px] rounded-[8px]
                 text-[14px]
-                
+                transition-all duration-200 ease-in-out
             `}
           >
             {SkinTypeLabel[msg.skinType]}
