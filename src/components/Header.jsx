691e1cb49b2a8a9646da3fa1945e2eb32bdb2bf0
장서현 <jsh030213@gmail.com>
2025-05-23 21:07:32 +0900

    feat: 페이지마다 로고 렌더링 다르게 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { ChatContext } from '../contexts/ChatContext';
 import { useLocation } from 'react-router-dom';
 import { IconLogo } from '../utils/icons';
+import { Logo } from '../utils/icons';
 import { useAuth } from '../contexts/AuthContext';
 
 import SidebarToggleButton from './SidebarToggleButton';
@@ -9,11 +10,13 @@ import FilterButton from './Header/FilterButton';
 import SummaryButton from './Header/SummaryButton';
 import HeaderProfile from './Header/HeaderProfile';
 import HeaderLoginButton from './Header/HeaderLoginButton';
+import { div } from 'framer-motion/client';
 
 const Header = ({ onClick }) => {
   const location = useLocation();
   const isChatPage = location.pathname.startsWith('/chat');
   const isLoginPage = location.pathname.startsWith('/login');
+  const isIndexPage = location.pathname === '/';
 
   // 추후에 로그인 정보 저장할 것
   const { isLoggedIn } = useAuth(); // ✅ 로그인 여부 확인
@@ -24,7 +27,14 @@ const Header = ({ onClick }) => {
       <div className=" flex items-center justify-between h-full w-full">
         {/* 왼쪽 버튼 */}
         <div className="">
-          {isChatPage ? <SidebarToggleButton /> : <img className="w-10" src={IconLogo} alt="" />}
+          {isChatPage && <SidebarToggleButton />}
+          {isLoginPage && <img className="w-10" src={IconLogo} alt="" />}
+          {isIndexPage && (
+            <div className="flex gap-1">
+              <img className="w-[39px]" src={IconLogo} alt="" />
+              <img className="w-[95%]" src={Logo} alt="" />
+            </div>
+          )}
         </div>
         {/* 조건부렌더링 */}
         <div className="flex w-full mx-auto items-center justify-between pl-[36px]">
