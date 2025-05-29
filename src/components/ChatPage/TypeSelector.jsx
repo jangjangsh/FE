9a6a69e2943599a7993ca6c55a55f4683cc4fcc8
장서현 <jsh030213@gmail.com>
2025-05-29 22:35:48 +0900

    style: 피부 타입 선택 버튼 중앙 정렬

--- src/components/ChatPage/TypeSelector.jsx
+++ src/components/ChatPage/TypeSelector.jsx
@@ -9,18 +9,19 @@ const TypeSelector = ({
   dropdownPositionClass,
 }) => {
   return (
-    <div className="relative inline-block w-full">
+    <div className="relative inline-block w-full justify-center">
       <button
         onClick={onClick}
         className="flex w-full px-[14px] py-[7px] rounded-[20px] gap-[6px] 
                   text-main font-semibold text-[14px]
                   bg-main-typeBackground hover:bg-white
+                  items-center
                   border-[1px] border-main-typeStroke hover:border-[rgba(59,104,239,0.1)]
                   
                   transition duration-300"
       >
-        <img className="w-[14px]" src={IconSmile} alt="피부" />
-        <span className="pr-[1px]">피부 타입</span>
+        <img className="w-[15px] items-center" src={IconSmile} alt="피부" />
+        <span className="items-center justify-center">피부 타입</span>
       </button>
       {isDropdownOpen && (
         <div className={`absolute ${dropdownPositionClass} left-0 w-full z-10`}>
